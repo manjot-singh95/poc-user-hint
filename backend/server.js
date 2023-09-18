3482ec38e44977d25edd3a52cd2cdc0252f054e6
Manjot Singh <manjotsingh@192.168.1.106>
2023-09-18 18:24:19 +0530

    added backend config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.set("Access-Control-Allow-Methods", header.Get("Allow"))
   res.set("Access-Control-Allow-Origin", "*")
   res.set("Accept-CH", "Sec-CH-UA, Sec-CH-UA-Form-Factor, Sec-CH-UA-Mobile, Sec-CH-UA-Model, Sec-CH-UA-Platform, Sec-CH-UA-Full-Version-List, Sec-CH-UA-Platform-Version")
   res.send('Hello World!');
