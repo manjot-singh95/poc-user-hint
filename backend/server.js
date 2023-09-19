b70dd0792c1aab629c1cb870c55576c525747ec7
Manjot Singh <manjotsingh@192.168.1.106>
2023-09-19 17:15:18 +0530

    frontend testing

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ var app = express();
 app.get('/', function (req, res) {
   res.set("Access-Control-Allow-Origin", "*")
   res.set("Accept-CH", "Sec-CH-UA, Sec-CH-UA-Form-Factor, Sec-CH-UA-Mobile, Sec-CH-UA-Model, Sec-CH-UA-Platform, Sec-CH-UA-Full-Version-List, Sec-CH-UA-Platform-Version")
-  res.send('Hello World!');
+  res.send({msg:'Hello World!'});
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
